12b7467336440573e2a3c0d13e4710be4899f09c
Prashant Chauhan <prashantchauhanabc@gmail.com>
2026-07-20 10:07:46 +0530

    Complete Integer Ranges

---
+++ LongLongDataType.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    long long x = LLONG_MAX;
+    long long y = LLONG_MIN;
+    cout<<x<<endl;
+    cout<<y<<endl;
+    return 0;
+}
---
+++ rangeofint.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x = INT_MAX;
+    cout<<x<<endl;
+    int y = INT_MIN;
+    cout<<y<<endl;
+    return 0;
+}
