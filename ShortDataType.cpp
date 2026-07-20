dd73aad1099fedc14bdc82934aa6395d7ca8e245
Prashant Chauhan <prashantchauhanabc@gmail.com>
2026-07-20 15:39:44 +0530

    Complete Integer Data Types

---
+++ ShortDataType.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<cstdint>
+using namespace std;
+int main(){
+    short x = INT16_MIN ;
+    short y = INT16_MAX;
+    cout<<x<<endl;
+    cout<<y<<endl;
+
+    return 0;
+}
