06aed9a71b8d7492a759e97a5025f647bba39dbb
Prashant Chauhan <prashantchauhanabc@gmail.com>
2026-07-29 15:27:14 +0530

    A to Z alphabets with Ascii Values

---
+++ AlphabetsAsciivalues.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter ch :";
+    cin>>n;
+    for(int i=65; i<=n; i++){
+        cout<<(char)i<< " "<<i<<endl;
+        
+    }
+
+
+    return 0;
+}
