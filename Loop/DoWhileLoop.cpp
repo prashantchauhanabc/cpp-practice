e1146e8eff980846a1d4083c6d00cc8d4f2d4b07
Prashant Chauhan <prashantchauhanabc@gmail.com>
2026-07-31 19:20:09 +0530

    While loop and Do While Loop

---
+++ Loop/DoWhileLoop.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i =11;
+    do{
+        cout<<i<< " ";
+        i++;
+    }
+    while(i<=15);
+    return 0;
+}
---
+++ Loop/whileLoop.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    int i =1;
+    cout<<"Enter a number: ";
+    cin>>n;
+    while(i<=n){
+        cout<<i<< " ";
+        i++;
+    }
+}
