bfe9b3d5b1004865324e68aba827ac14aa55dd7e
Prashant Chauhan <prashantchauhanabc@gmail.com>
2026-07-31 13:05:52 +0530

    Count Factors and Prime Or Composite Number

---
+++ CountFactors.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter a number: ";
+    cin>>n;
+    int factors =0;
+    for(int i=1; i<=n; i++){
+        if(n%i == 0){
+        factors++;
+        }
+    }
+    if(factors >=3){
+        cout<<"Composite numbers";
+    }
+    else{
+        cout<<"Prime Number";
+    }
+
+    return 0;
+}
